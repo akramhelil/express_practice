da04eedbe7d35147ac3fb2023bea5d79d74d407f
akramhelil <akramhelil@gmail.com>
2019-07-23 16:03:30 -0400

    played around with routes

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var app = express()
 
 // Routes 
 // "/" => Hi 
+
+
 app.get("/", (req,res) => {
     res.send("Hi there!")
 })
-
-
 
 
 // "/bye" => "Good Bye"
@@ -23,6 +23,19 @@ app.get("/cat", (req,res) => {
     res.send('nodemon test route')
 })
 
+// Route Parames 
+app.get("/cat/:id/comments", (req, res) => {
+    console.log(req)
+    res.send('this is the paramsa route')
+})
+
+// ORDER of the routes matters
+app.get("*", (req,res) => {
+    res.send("this is the star server maybe use for 404 page")
+})
+
+
+//  Tells express to start listening the port, nodemon installed for the autp hot load 
 app.listen(8000, () => {
     console.log("Listening", 8000)
 })
