b2d6eb2697a7bceaaafa8411664063da6bd956cf
akramhelil <akramhelil@gmail.com>
2019-07-23 15:27:13 -0400

    routes practice

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Express App Goes here")
+var express = require('express')
+var app = express()
+
+// Routes 
+// "/" => Hi 
+app.get("/", (req,res) => {
+    res.send("Hi there!")
+})
+
+
+
+
+// "/bye" => "Good Bye"
+app.get('/bye', (req,res) => {
+    res.send("Bye Take care!")
+})
+
+// "/dog"
+app.get("/dog", (req,res) => {
+    res.send('Woof! Woof!')
+})
+
+app.listen(8000, () => {
+    console.log("Listening", 8000)
+})
