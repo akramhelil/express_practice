6db3cea2b4bc8f514f9ceb6b3dcc18f09e30ca02
akramhelil <akramhelil@gmail.com>
2019-07-24 12:04:35 -0400

    ejs passing data practice

--- day2/app.js
+++ day2/app.js
@@ -8,6 +8,12 @@ app.get("/", (req, res) => {
     // res.send("<h1>Loaded<h1>")
 })
 
+app.get('/fallinlovewith/:thing', (req,res) => {
+    let thing = req.params.thing
+    res.render("love.ejs", {thingVar: thing})
+})
+
+
 
 app.listen(PORT, () => {
     console.log("Server is Listening on:", PORT)
