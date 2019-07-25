3b3ade5b97c7595518d17523286012561d086ca6
akramhelil <akramhelil@gmail.com>
2019-07-25 17:56:57 -0400

    another commit for NodeJS added Post

--- day2/app.js
+++ day2/app.js
@@ -10,9 +10,19 @@ app.get("/", (req, res) => {
 
 app.get('/fallinlovewith/:thing', (req,res) => {
     let thing = req.params.thing
+  
     res.render("love.ejs", {thingVar: thing})
 })
 
+app.get('/posts', (req,res) => {
+    let posts = [
+        {title: "Post 1", author: "Akram"},
+        {title: "My cure D0gs", author: "ron"},
+        {title: "Adoreable Handsome Sultan", author: "Cat"},
+        {title: "I love dogs", author: "Sul"}
+    ]
+    res.render("posts.ejs", {posts: posts})
+})
 
 
 app.listen(PORT, () => {
