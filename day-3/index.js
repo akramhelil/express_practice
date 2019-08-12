b778a902297ffe17109d976438290435d4714703
akramhelil <akramhelil@gmail.com>
2019-08-12 10:11:45 -0400

    POST resquest Router practice on POSTMAN

--- day-3/index.js
+++ day-3/index.js
@@ -1,4 +1,21 @@
 const express = require('express');
 var app = express();
 
-app.set
+app.set('view engine', 'ejs')
+
+app.get('/', (req,res) => {
+    res.render('home')
+})
+app.get('/friends', (req, res) => {
+    let friendsArr =['Justin', 'Akram', 'Chris', 'Ron']
+    res.render('friends', {friends: friendsArr})
+})
+// POST
+app.post('/addfriend', (req,res) => {
+    res.send('You have reached the Post Route')
+})
+
+
+app.listen(3000, () => {
+    console.log('Starting the server')
+})
